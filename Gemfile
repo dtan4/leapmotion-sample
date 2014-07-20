b694120991eec4ea2c7a530cdbcc606c22e4102b
dtan4 <dtanshi45@gmail.com>
2014-07-20 23:36:24 +0900

    Load assets

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-gem "coffee"
+gem "coffee-script"
 gem "sass"
 gem "sinatra", "~> 1.4", require: "sinatra/base"
+gem "sinatra-assetpack", require: "sinatra/assetpack"
 gem "slim"
--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 class App < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
+  register Sinatra::AssetPack
+
+  assets do
+    serve "/js", from: "assets/javascripts"
+    serve "/css", from: "assets/stylesheets"
+
+    js :application,  [
+      "/js/*.js"
+    ]
+
+    css :application, [
+      "/css/*.css"
+    ]
+
+    css_compression :sass
+  end
+
   get "/" do
-    "leapmotion"
+    slim :index
   end
 end
